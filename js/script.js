ea7ee39b01cc6d2a1b1311bc2f0f0cc55a31c177
Renan Gois de Almeida <Renan_Almeida6@outlook.com>
2022-07-21 22:19:35 -0300

    Ajuste

--- js/script.js
+++ js/script.js
@@ -63,7 +63,7 @@ const loop = setInterval(() => {
     mario.style.animation = 'none'
     mario.style.bottom = `${marioPositon}px`
 
-    mario.src = './imagens/game-over.png'
+    mario.setAttribute('src', './imagens/game-over.png')
     mario.style.width = '75px'
     mario.style.marginLeft = '50px'
 
